38698ffac5291affa38632e0232f1511a83d9159
Anannyap7 <anannyapopat20@gmail.com>
2021-05-09 13:35:17 +0530

    Added comments

--- app/src/main/java/com/example/animationbutton/MainActivity.java
+++ app/src/main/java/com/example/animationbutton/MainActivity.java
@@ -125,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Move
         Move.setOnClickListener(new View.OnClickListener()
         {
             @Override
